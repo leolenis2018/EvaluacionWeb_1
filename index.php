4d81e98bdddd73305a391d76b02670cff83bf989
LeoLenis <leolenis2018@gmail.com>
2020-10-02 19:15:36 -0500

    parte 6

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Repasos Php</title>
+    <title>Evaluacion Web I</title>
     <link rel="stylesheet" href="CSS/estilos.css">
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
 </head>
